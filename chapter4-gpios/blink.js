4b755a69a61f6fe5d34e0887ba98a23b4af5b145
ely_az <joshua.ely.williams@gmail.com>
2016-07-09 16:56:43 +0000

    Github Test

--- chapter4-gpios/blink.js
+++ chapter4-gpios/blink.js
@@ -19,6 +19,7 @@ process.on('SIGINT', function () { //#F
   process.exit();
 });
 
+// Does this update?
 // #A Import the onoff library
 // #B Initialize pin 4 to be an output pin
 // #C This interval will be called every 2 seconds
